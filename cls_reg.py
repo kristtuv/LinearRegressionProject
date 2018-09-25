b04d422fd45ba598f7d145c515c4203952512403
Tommy <tommy.myrvik@fys.uio.no>
2018-09-25 16:45:28 +0200

    Prøver igjen...

--- cls_reg.py
+++ cls_reg.py
@@ -6,19 +6,36 @@ from sklearn.linear_model import LinearRegression, RidgeCV, Lasso
 
 class LinReg:
     def __init__(self, x, y, z, deg):
+        """
+        :param XY: A matrix of polynomialvalues
+        :param z: The values we are trying to fit
+        :param deg: The degree of polynomial we try to fit the data
+        :type XY: array
+        :type z: array
+        :type deg: int
+        """
 
         self.x = x
         self.y = y
         self.z = z
         self.deg = deg
         self.N = x.shape[0]
-        self.lamb = 0.1
 
         xy = np.append(x, y, axis=1)
         poly = PolynomialFeatures(degree = deg)
         self.XY = poly.fit_transform(xy)
 
     def ols(self, XY = None, z = None):
+        """
+        Performes a Ordinary least squares linear fit
+
+        :param XY: A matrix of polynomialvalues
+        :param z: The values we are trying to fit
+        :type XY: array
+        :type z: array
+        :return: The coefficient of the fitted polynomial
+        :rtype: array
+        """
 
         if XY is None: XY = self.XY
         if z is None: z = self.z
@@ -26,13 +43,24 @@ class LinReg:
         #beta = scl.inv(XY.T @ XY) @ XY.T @ z
         beta = np.linalg.pinv(XY) @ z
 
-        zpredict = XY @ beta
-        varz = 1.0/(XY.shape[0] - self.deg - 1)*np.sum((z - zpredict)**2)
-        self.var_ols = np.linalg.pinv(XY.T @ XY)*varz
+        self.varz = np.var(z)
+        self.var_ols = np.linalg.pinv(XY.T @ XY)*self.varz
 
         return beta
 
     def ridge(self, lamb, XY = None, z = None):
+        """
+        Performes a Ridge regression linear fit
+
+        :param XY: A matrix of polynomialvalues
+        :param z: The values we are trying to fit
+        :param lamb: The regularization constant
+        :type XY: array
+        :type z: array
+        :type lamb: float, int
+        :return: The coefficient of the fitted polynomial
+        :rtype: array
+        """
 
         if XY is None: XY = self.XY
         if z is None: z = self.z
@@ -44,6 +72,18 @@ class LinReg:
 
 
     def lasso(self, lamb, XY = None, z = None):
+        """
+        Performes a Lasso regression linear fit
+
+        :param XY: A matrix of polynomialvalues
+        :param z: The values we are trying to fit
+        :param lamb: The regularization constant
+        :type XY: array
+        :type z: array
+        :type lamb: float, int
+        :return: The coefficient of the fitted polynomial
+        :rtype: array
+        """
 
         if XY is None: XY = self.XY
         if z is None: z = self.z
@@ -58,11 +98,30 @@ class LinReg:
 
 
     def MSE(self, z, zpred):
+        """
+        Finds the mean squared error of the real data and predicted values
+        :param z: real data
+        :param zpred: predicted data
+        :type z: array
+        :type zpred: array
+        :return: The mean squared error
+        :rtype: float
+        """
 
         return 1.0/z.shape[0]*np.sum((z - zpred)**2)
 
 
     def R2(self, z, zpred):
+        """
+        Finds the R2 error of the real data and predicted values
+        :param z: real data
+        :param zpred: predicted data
+        :type z: array
+        :type zpred: array
+        :return: The mean squared error
+        :rtype: float
+        """
+
 
         zmean = np.average(z)
 
@@ -70,6 +129,9 @@ class LinReg:
 
 
     def bootstrap(self, nBoots):
+        """
+        I dont fucking know
+        """
 
         boot_mse = np.zeros(nBoots)
         betas = np.zeros((nBoots, self.XY.shape[1]))
@@ -79,7 +141,7 @@ class LinReg:
             XY = self.XY[idx]
             z = self.z[idx]
 
-            beta = self.ols(XY, z)
+            beta = self.ols()
             betas[i] = beta.flatten()
             zpredict = XY @ beta
             mse = self.MSE(z, zpredict)
@@ -89,8 +151,8 @@ class LinReg:
         beta_var = np.var(betas, axis = 0)
 
         self.boot_mse = np.average(boot_mse)
-        self.boot_var = np.var(boot_mse)
-
+        self.boot_ave = np.var(boot_mse)
+        
         print("Average MSE after %i resamples: " %(nBoots), self.boot_mse)
         print("Variance of MSE after %i resamples: " %(nBoots), self.boot_var)
         print("\nAverage betas after %i resamples: \n" %(nBoots), beta_ave)
@@ -98,6 +160,10 @@ class LinReg:
 
 
     def kfold(self, nfolds):
+        """
+        I dont fucking know
+        """
+
 
         XY_folds = np.array_split(self.XY, nfolds, axis = 0)
         z_folds = np.array_split(self.z, nfolds, axis = 0)
