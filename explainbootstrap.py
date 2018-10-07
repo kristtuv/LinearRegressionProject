3e70e76a3ff8eeb9a51d8f0779861d67d28d5589
Kristian <ktuv@hotmail.com>
2018-10-07 19:52:30 +0200

    Created split_patches

---
+++ explainbootstrap.py
@@ -0,0 +1,54 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+import plotparams
+N = 3
+x = np.linspace(-5, 5, 15).reshape(-1, 1)
+poly = PolynomialFeatures(degree=2)
+X = poly.fit_transform(x)
+linreg = LinearRegression()
+yvecdata = np.zeros((N, x.shape[0]))
+
+fig, ax = plt.subplots(2, 1, sharey=True)
+ax[0].set_ylabel('Y')
+ax[1].set_ylabel('Y')
+print(np.random.randint(6))
+for i in range(N):
+    noise = np.random.randn(len(x)).reshape(-1,1)
+    y = x**2 + 10*noise
+    linreg.fit(X, y)
+    ypredict= linreg.predict(X)
+    yvecdata[i] = ypredict.flatten()
+    ax[0].scatter(x, y)
+    ax[0].plot(x, ypredict, linewidth=1)
+ax[0].plot(x,np.mean(yvecdata, axis = 0).reshape(-1, 1), color = 'green', linewidth=2, label='Average')
+ax[0].set_title('Random datasets')
+
+N = 5
+# x = np.linspace(-5, 5, 100gcc).reshape(-1, 1)
+noise = np.random.randn(len(x)).reshape(-1,1)
+y = x**2 + 10*noise
+print(y.shape)
+poly = PolynomialFeatures(degree=2)
+X = poly.fit_transform(x)
+print(X.shape)
+linreg = LinearRegression()
+yvecboot = np.zeros((N, x.shape[0]))
+for i in range(N):
+    choice = np.random.choice(x.shape[0], x.shape[0])
+    linreg.fit(X[choice], y[choice])
+    ypredict= linreg.predict(X)
+    yvecboot[i] = ypredict.flatten()
+    # if symbols[i] == '.':
+    #     plt.scatter(x[choice], y[choice], marker=symbols[i],  color=colors[i], linewidth=0.8, s = 70)
+    # else:
+    #     plt.scatter(x[choice], y[choice], marker=symbols[i],  color=colors[i], linewidth=0.8, s = 500)
+    ax[1].plot(x, ypredict, linewidth = 1)
+ax[1].scatter(x, y, label='Original Data')
+ax[1].plot(x,np.mean(yvecboot, axis = 0).reshape(-1, 1), color='green',linewidth=2, label='Average')
+ax[1].set_title('Bootstrap')
+plt.xlabel('X')
+plt.tight_layout()
+plt.savefig('plots/bootstrapexplain.png')
+plt.show()
---
+++ split_patches.py
@@ -0,0 +1,40 @@
+import numpy as np
+def split_patches(self, x, y, z, colsplit, rowsplit, choose_patches):
+    """
+    param: colsplit: How many column(c) do you want the data split into
+    param: rowsplit: How many rows(r) do you want the data split into
+    param: choose_patches: Number of patches your want to keep. Can not be bigger than [c x r]
+    type: x: np.ndarray
+    type: y: np.ndarray
+    type: z: np.ndarray
+    typ: colsplit: int
+    typ: rowsplit: int
+    typ: choose_patches: int
+    """
+
+    x,y = np.meshgrid(x,y)
+
+    ###Spliting
+    ###The
+    ###Data
+    zcolsplit = np.array_split(z, colsplit, axis=1)
+    xcolsplit = np.array_split(x, colsplit, axis=1)
+    ycolsplit = np.array_split(y, colsplit, axis=1)
+    zsplit = [np.array_split(np.vstack(zcolsplit[i]), rowsplit) for i in range(len(zcolsplit))]
+    xsplit = [np.array_split(np.vstack(xcolsplit[i]), rowsplit) for i in range(len(xcolsplit))]
+    ysplit = [np.array_split(np.vstack(ycolsplit[i]), rowsplit) for i in range(len(ycolsplit))]
+
+    ###Choosing random indices
+    ## and making sure no set of 
+    ###indices are equal
+    np.random.choice(rowsplit, choose_patches)
+    randomset =  []
+    while len(frozenset(randomset)) < choose_patches:
+        a = np.random.choice(colsplit)
+        b = np.random.choice(rowsplit)
+        randomset.append((a,b))
+
+    self.z = np.concatenate([zsplit[randset[0]][randset[1]].reshape(-1,1) for randset in randomset])
+    self.x = np.concatenate([xsplit[randset[0]][randset[1]].reshape(-1,1) for randset in randomset])
+    self.y = np.concatenate([ysplit[randset[0]][randset[1]].reshape(-1,1) for randset in randomset])
+    return x, y, z
