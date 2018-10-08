9d9cfa06b3e546913d1c1a59c84348b059586970
Tommy <tommy.myrvik@fys.uio.no>
2018-10-09 00:08:05 +0200

    some clean

--- lasso.py
+++ lasso.py
@@ -2,13 +2,13 @@ import numpy as np
 from cls_reg import LinReg
 from Franke import FrankeFunction
 import matplotlib.pylab as plt
-import plotparams
+#import plotparams
 
 N = 100
 degrees = range(10)
 #degrees = [5]
 #lambdas = [10**i for i in range(-3, 3)]
-lambdas = [0.1, 10.0, 100]
+lambdas = [0.001, 0.01, 0.1]
 #lambdas = [0.1]
 
 x = np.random.uniform(0, 1, (N, 1))
@@ -19,7 +19,7 @@ z = FrankeFunction(x, y) + noise
 conf = np.zeros((len(lambdas), len(degrees)))
 
 
-print("RIDGE REGRESSION")
+print("LASSO REGRESSION")
 print("| Degree | lambda | MSE Train | MSE Test |  R2 Train | R2 Test | Conf.int.|")
 print("="*75)
 for i in range(len(degrees)):
@@ -29,7 +29,7 @@ for i in range(len(degrees)):
     for j in range(len(lambdas)):
 
         a.lamb = lambdas[j]
-        beta = a.ridge(a.XY_Train, a.z_Train)
+        beta = a.lasso(a.XY_Train, a.z_Train)
         zpred_train = a.XY_Train @ beta
         zpred_test = a.XY_Test @ beta
 
@@ -37,12 +37,22 @@ for i in range(len(degrees)):
         mse_test = a.MSE(a.z_Test, zpred_test)
         r2_train  = a.R2(a.z_Train, zpred_train)
         r2_test  = a.R2(a.z_Test, zpred_test)
-        conf_int = np.average(a.conf_ridge)
+
+        a.bootstrap(100, a.lasso)
+        conf_int = np.average(1.96*np.sqrt(a.betas_var))
 
         conf[j, i] = conf_int
 
+        if degrees[i] == 3:
+            a.lamb = 0.1
+            beta_ridge = a.ridge(a.XY_Train, a.z_Train)
+            beta_ols = a.ols(a.XY_Train, a.z_Train)
+            print("\nOLS:\n", beta_ols.flatten())
+            print("\nRIDGE:\n", beta_ridge.flatten())
+            print("\nLASSO:\n", beta.flatten())
+
         print("|%8i|%8g|%11f|%10f|%10f|%10f|%10f|" % \
-            (degrees[i], lambdas[j], mse_train, mse_test, r2_train, r2_test, np.average(a.conf_ridge)))
+            (degrees[i], lambdas[j], mse_train, mse_test, r2_train, r2_test, conf_int))
     print("-"*75)
 
 labels = [r"$\lambda=$" + str(x) for x in lambdas]
@@ -53,7 +63,7 @@ plt.ylabel("CI(95%)")
 plt.legend(labels)
 plt.show()
 
-lambdas = np.array([5*i + 1.0 for i in range(20)])
+lambdas = np.array([0.01*i + 0.001 for i in range(20)])
 noises = np.array([0.05*i for i in range(20)])
 mses = np.zeros((len(noises), len(lambdas)))
 deg = 3
@@ -65,7 +75,7 @@ for i in range(len(noises)):
     for j in range(len(lambdas)):
 
         a.lamb = lambdas[j]
-        beta = a.ridge(a.XY_Train, a.z_Train)
+        beta = a.lasso(a.XY_Train, a.z_Train)
         zpred_test = a.XY_Test @ beta
         mse_test = a.MSE(a.z_Test, zpred_test)
         mses[i, j] = mse_test
@@ -75,7 +85,7 @@ extent = np.min(lambdas), np.max(lambdas), np.min(noises), np.max(noises)
 
 plt.imshow(mses, aspect = "auto", extent = extent)
 plt.colorbar()
-plt.title(r"MSE($\lambda$, $\sigma$), Ridge Deg %i" %(deg), fontsize=16)
+plt.title(r"MSE($\lambda$, $\sigma$), Lasso Deg %i" %(deg), fontsize=16)
 plt.xlabel(r"$\lambda$", fontsize=14)
 plt.ylabel(r"$\sigma$", fontsize=14)
 plt.show()
